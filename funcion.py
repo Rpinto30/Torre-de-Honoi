94e930c0adbca2acc924098a5c063b755ae1b86b
Rpinto30 <rodrigopinto3007@gmail.com>
2025-09-26 10:23:42 -0600

    AAAAAAAAAAAAAAAAAAAAAAAAAH

--- funcion.py
+++ funcion.py
@@ -1,30 +1,31 @@
-def torre_hanoi(n, i = 0, j=1, k = 3):
+def torre_hanoi(n, i=0, j=1, k=3, w=['A','B','C']):
     text = ''
-    if i >= (2**n)-1: print("Torre resuelta")
+    if i >= (2**n) - 1:
+        print("Torre resuelta")
+        return
     else:
-        if i < n//2:  w = ['A', 'C', 'B']
-        else:  w = ['B', 'A', 'C']
-        if j%2 != 0:
-            text += 'Mover 1 a'
-            if j==4: torre_hanoi(n,i+1,j+1, k+1)
-            else:torre_hanoi(n,i+1,j+1, k)
-
-        elif j%2 == 0:
-            if j != 4:
-                text += 'Mover 2 a'
-                torre_hanoi(n,i+1,j+1, k)
+        if j % 2 != 0:
+            text += f"Mover 1 a {w[(i % 2) + 1]}"
+            if j == 4:
+                torre_hanoi(n, i+1, j+1, k+1, w)
             else:
-                if i != ( (2**n)-1)//2:
-                    text += f'Mover {k} a'
+                torre_hanoi(n, i+1, j+1, k, w)
+        elif j % 2 == 0:
+            if j != 4:
+                text += f"Mover 2 a {w[(i % 3)]}"
+                torre_hanoi(n, i+1, j+1, k, w)
+            else:
+                if i != (2**n - 1)//2:
+                    text += f"Mover {k} a {w[(i//k) % 3]}"
                     if k == n-1:
-                        torre_hanoi(n, i + 1, 1, 3)
+                        torre_hanoi(n, i+1, 1, 3, w)
                     else:
-                        torre_hanoi(n, i + 1, 1, k+1)
+                        torre_hanoi(n, i+1, 1, k+1, w)
                 else:
-                    text = f'Mover {n} a C'
-                    torre_hanoi(n, i + 1, 1, 3)
+                    text = f"Mover {n} a C"
+                    torre_hanoi(n, i+1, 1, 3, w)
+    print(i, text)
 
-    print(text)
-    #print(i,j,k)
-torre_hanoi(5)
-print(31//2)
+
+# Prueba
+torre_hanoi(3)
